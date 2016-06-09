e057f0554ff831fb895200e95c1c5b4e440c7b12
James Christopher Adduono <jc@adduono.com>
2016-06-08 22:16:48 -0400

    Fixes & Cleanups
    - Mark userdata as footer encryptable
    - Enable bootdevice symlink folder
    - Disable USB mass storage (missing lun & external sdcard)
    - Uniform styling in BoardConfig.mk
    - Rename kernel to Image.gz to retain understandable format
    - Move recovery.fstab to root, remove unnecessary flag
    - Don't build toolbox, we don't need it
    - Set TW_THEME instead of resolution (automatic)
    - Remove system.prop that is doing nothing at all
    
    Change-Id: I67c3f0f15f34c0621aa4258d2af2f1ea9c3fd4fd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,7 @@
 
 LOCAL_PATH := device/xiaomi/hennessy
 
-
-# Architecture
+# ARCHITECTURE
 TARGET_ARCH := arm64
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := armeabi-v7a
@@ -32,28 +31,24 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 
 TARGET_CPU_SMP := true
 
+# COMPILER FLAGS
+#TARGET_GLOBAL_CFLAGS += 
+#TARGET_GLOBAL_CPPFLAGS += 
+
 # BOOTLOADER
 TARGET_BOARD_PLATFORM := mt6795
 TARGET_BOOTLOADER_BOARD_NAME := mt6795
 TARGET_NO_BOOTLOADER := true
 
-# COMPILER FLAGS
-TARGET_GLOBAL_CFLAGS += 
-TARGET_GLOBAL_CPPFLAGS += 
-
-
-# Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x40080000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS  := --kernel_offset 0x00000000 --ramdisk_offset 0x03f80000 --tags_offset 0x0df80000
-TARGET_PREBUILT_KERNEL := device/xiaomi/hennessy/kernel
+# KERNEL
+BOARD_KERNEL_CMDLINE   := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive androidboot.bootdevice=mtk-msdc.0
+BOARD_KERNEL_BASE      := 0x40080000
+BOARD_KERNEL_PAGESIZE  := 2048
+BOARD_MKBOOTIMG_ARGS   := --kernel_offset 0x00000000 --ramdisk_offset 0x03f80000 --tags_offset 0x0df80000
+TARGET_PREBUILT_KERNEL := device/xiaomi/hennessy/Image.gz
 
 # TARGET IMAGES
 TARGET_USERIMAGES_USE_EXT4 := true
-
-# FSTAB LOCATIONS
-TARGET_RECOVERY_FSTAB := device/xiaomi/hennessy/recovery/root/etc/recovery.fstab
 
 # PARTTIONS
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
@@ -62,13 +57,12 @@ BOARD_FLASH_BLOCK_SIZE := 0x20000
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 
-# TWRP stuff
-DEVICE_RESOLUTION := 1080x1920
-TW_NO_CPU_TEMP := true
+# TWRP FLAGS
+TW_THEME := portrait_hdpi
 TW_INCLUDE_CRYPTO := true
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_NO_CPU_TEMP := true
+TW_NO_USB_STORAGE := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_EXCLUDE_SUPERSU := true
-TW_USE_TOOLBOX := true
